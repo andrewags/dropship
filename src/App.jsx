e35ad3b7d88b73ab7d79ba6274d4d029083abda0
Shubin123 <wangshubin130@gmail.com>
2022-05-10 10:36:02 -0700

    build changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,117 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import './styles.scss'
+import Grid from '@mui/material/Grid'
+import { Routes, Route, Link } from "react-router-dom"
+import instagram from '/Instagram-Logo.wine.svg'
+import facebook from '/Facebook-f_Logo-Blue-Logo.wine.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+
+
+
+
+function Home(){
+  const [width, setWidth] = useState(window.innerWidth);
+
+function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+}
+
+useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+        window.removeEventListener('resize', handleWindowSizeChange);
+    }
+}, []);
+
+const isMobile = width <= 768;
+
+return(
+     <Grid container spacing={2}
+  direction = {isMobile ? 'column' : 'row'}
+  alignItems= {isMobile ? "center" : "flex-start"}
+  justifyContent= {isMobile ? "center" : "flex-start"}
+  style={{ minHeight: '100vh' }}>
+  <Grid item xs={8}>
+    <h1>Homepage</h1>
+    <h2>sub heading</h2>
+    <h2>sub-sub</h2>
+  </Grid>
+  <Grid item xs={4}>
+  <h1>2</h1>
+
+  </Grid>
+  <Grid item xs={4}>
+  <h1>3</h1>
+
+  </Grid>
+  <Grid item xs={8}>
+  <h1>4</h1>
+  <h2 className='alignr'>right aligned text</h2>
+  </Grid>
+  <Grid item xs={8}>
+  <h1>5</h1>
+  <nav>
+        <Link to="/about">About</Link>
+    </nav>
+  </Grid>
+  <Grid item xs={4}>
+  <h1>6</h1>
+  <nav>
+        <Link to="/contact">Contact</Link>
+    </nav>
+
+  </Grid>
+  <Grid item xs={4} className = "social" >
+  <div className='social'>
+  <h1>7</h1>
+  <h2>social</h2>
+  <img src={instagram} alt="instagram" className='instagram'/>
+  <img src={facebook} alt="facebook" className='facebook'/>
+  </div>
+  
+  </Grid>
+  
+  <Grid item xs={8}>
+    <h1>8</h1>
+  </Grid>
+
+  </Grid>
+  )
+}
+
+function About(){
+  return(
+    <div>
+      <h1>About Page</h1>
+      <nav>
+        <Link to="/">back to homepage</Link>
+    </nav>
     </div>
   )
 }
 
+function Contact(){
+  return(
+    <div>
+      <h1>Contact Page</h1>
+      <nav>
+        <Link to="/">back to homepage</Link>
+    </nav>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  )
+}
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as ReactDOM from "react-dom"
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App /> 
+  </BrowserRouter>
+  ), document.getElementById('root')
+);
